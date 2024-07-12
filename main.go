85fcd9f6e3abcdb0b57dc2067c87d8e1fd9cd02a
Elnakib <kareemahmedelnakib04@gmail.com>
2024-07-12 03:29:18 +0300

    added os packages

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import (
+	"os"
+)
+
 var N string = "Novels"
 var ph string = "Philosophy"
 var H string = "History"
 var oop string = "oop"
 var DSA string = "Data Structure"
+
+var oc = os.Create
 
 func scanPdf() {
 
