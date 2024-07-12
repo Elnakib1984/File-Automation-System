8a2098d7bda2aa3048ee69732c6ffadb640597bd
Elnakib <kareemahmedelnakib04@gmail.com>
2024-07-12 22:09:23 +0300

    update

--- main.go
+++ main.go
@@ -8,24 +8,10 @@ import (
 	"strings"
 )
 
-var N string = "Novels"
-var ph string = "Philosophy"
-var H string = "History"
-var oop string = "oop"
-var DSA string = "Data Structure"
-
-var oc = os.Create
-
-var pl = fmt.Println
-
-func scanPdf() {
-
-}
-
 func main() {
 
-	srcDir := "E:/semester 2/m213" //source
-	destDir := "E:/book/oop"       //destination
+	srcDir := "E:/semester 2/m213"
+	destDir := "E:/book/oop"
 
 	if _, err := os.Stat(destDir); os.IsNotExist(err) {
 		os.MkdirAll(destDir, os.ModePerm)
@@ -54,34 +40,3 @@ func main() {
 
 	fmt.Println("PDF files ending with 'oop' moved successfully!")
 }
-
-/*
-
-Some git commands
-
-# Step 1: Clone the main repository
-git clone https://github.com/main-repo.git
-cd main-repo
-
-# Step 2: Add the fork as a remote
-git remote add fork https://github.com/your-forked-repo.git
-
-# Step 3: Fetch the fork
-git fetch fork
-
-# Step 4: Check out the master branch
-git checkout master
-
-# Step 5: Merge the fork into master
-git merge fork/branch-name
-
-# Step 6: Resolve any conflicts
-# Open files, make necessary changes, then
-git add conflict-file
-
-# Step 7: Commit the merge
-git commit
-
-# Step 8: Push the changes
-git push origin master
-*/
