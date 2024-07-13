caa6dd98ee03e6954e81a88da0d1024dbef8947f
Elnakib <kareemahmedelnakib04@gmail.com>
2024-07-13 16:04:18 +0300

    update

--- src/scan.go
+++ src/scan.go
@@ -9,18 +9,18 @@ import (
 )
 
 func main() {
-	root := "/" 
+	root := "/"
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			
+
 			log.Printf("error accessing path %s: %v\n", path, err)
 			return nil // skip the directory or file causing the error
 		}
 		if !info.IsDir() && strings.HasSuffix(info.Name(), ".pdf") {
 			fmt.Println("Found PDF:", path) // print the file path if it is a PDF
 		}
-		return nil 
+		return nil
 	})
 
 	if err != nil {
